63fcaceee7b37038efa201c5a6da9f59bf163549
Dmitry Yurashevich <dmitry.yurashevich@itechart-group.com>
2021-10-25 20:38:25 +0300

    init project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { Component, StrictMode } from "react";
+import ReactDom from "react-dom";
+
+class AppContainer extends Component<Record<string, never>, Record<string, never>> {
+  render() {
+    return (
+      <StrictMode>
+        <div>Hello world</div>
+      </StrictMode>
+    );
+  }
+}
+
+ReactDom.render(<AppContainer />, document.getElementById("app"));
