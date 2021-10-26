e56aece508720d3eb2654c5dbc1c8e208b8d8ad0
Dmitry Yurashevich <dmitry.yurashevich@itechart-group.com>
2021-10-26 19:08:56 +0300

    test husky

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ class AppContainer extends Component<Record<string, never>, Record<string, never
   render() {
     return (
       <StrictMode>
-        <div>Hello world</div>
+        <div>Hello world!!!</div>
       </StrictMode>
     );
   }
